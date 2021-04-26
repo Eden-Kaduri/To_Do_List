f1944a805bfeb6efe1dd0cd0be5bf2c3a929f5fc
Eden Kaduri <edenkadouri@gmail.com>
2021-04-26 19:15:25 +0300

    Added "Delete Tasks" feature

--- app.js
+++ app.js
@@ -1,39 +1,64 @@
-const form=document.querySelector('#task-form');
-const taskList=document.querySelector('.collection');
-const clearBtn=document.querySelector('.clear-tasks');
-const filter=document.querySelector('#filter');
-const taskInput= document.querySelector('#task');
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
 
 
 loadAllEventListeners();
 //Loading event listeners
-function loadAllEventListeners(){
+function loadAllEventListeners() {
     form.addEventListener('submit', addTask);
+    //Remove task event
+    taskList.addEventListener('click', removeTask);
+    //Remove All tasks
+    clearBtn.addEventListener('click', clearAllTasks);
+
 }
 
 
-function addTask(e){
-    if(taskInput.value === ''){
+function addTask(e) {
+    if (taskInput.value === '') {
         alert('Add a task');
     }
     //Create li element
-    const li=document.createElement('li');
+    const li = document.createElement('li');
     //Add class
-    li.className='collection-item';
+    li.className = 'collection-item';
     //Create text node
     li.appendChild(document.createTextNode(taskInput.value));
     //Create new link
-    const link=document.createElement('a');
+    const link = document.createElement('a');
     //Add class to link
-    link.className='delete-item secondary-content';
+    link.className = 'delete-item secondary-content';
     //Add icon
-    link.innerHTML='<i class="fas fa-trash-alt"></i>';
+    link.innerHTML = '<i class="fas fa-trash-alt"></i>';
     //Append the link to li
     li.appendChild(link);
 
     taskList.appendChild(li);
 
-    taskInput.value='';
+    taskInput.value = '';
 
     e.preventDefault();
 }
+
+
+//Remove Task
+function removeTask(e) {
+    if (e.target.parentElement.classList.contains('delete-item')) {
+        if (confirm('Are you Sure?')) {
+            e.target.parentElement.parentElement.remove();
+        }
+    }
+}
+
+//Clear tasks
+function clearAllTasks(e) {
+    if (confirm('Clear all tasks?')) {
+        while (taskList.firstChild) {
+            taskList.removeChild(taskList.firstChild);
+        }
+    }
+}
+
